485c87bc24763ccefc8b255a6470239c05c5f7ad
James Jenkins <applesmacs@yahoo.com>
2015-10-29 03:42:34 -0400

    Add matricies for testing rref
    - Comment out other code for dynamic matrix generation and entry

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
+	//	"strconv"
 	"strings"
 )
 
@@ -17,31 +17,71 @@ func prompt(prmpt string) string {
 }
 
 func main() {
-	var dimensions []string
+	//var dimensions []string
 	var rows, cols int
+	rows = 3
+	cols = 3
 
-	for len(dimensions) != 2 {
-		var err1, err2 error
-		dim_string := (prompt("Matrix dimenstions: "))
-		// Replace "," with "x" so user can enter J,K or JxK
-		dim_string = strings.Replace(dim_string, ",", "x", 1)
-		dimensions = strings.Split(dim_string, "x")
+	/*
+		for len(dimensions) != 2 {
+			var err1, err2 error
 
-		if len(dimensions) != 2 {
-			fmt.Println("Please enter matrix dimensions as JxK.")
-			continue
+			dim_string := prompt("Matrix dimenstions: ")
+			// Replace "," with "x" so user can enter J,K or JxK
+			dim_string = strings.Replace(dim_string, ",", "x", 1)
+			dimensions = strings.Split(dim_string, "x")
+
+			if len(dimensions) != 2 {
+				fmt.Println("Please enter matrix dimensions as JxK.")
+				continue
+			}
+
+			rows, err1 = strconv.Atoi(dimensions[0])
+			cols, err2 = strconv.Atoi(dimensions[1])
+
+			if err1 != nil || err2 != nil {
+				fmt.Println("Please use integers for array dimensions.")
+			}
 		}
-
-		rows, err1 = strconv.Atoi(dimensions[0])
-		cols, err2 = strconv.Atoi(dimensions[1])
-
-		if err1 != nil || err2 != nil {
-			fmt.Println("Please use integers for array dimensions.")
-		}
-	}
+	*/
 
 	m := newMatrix(rows, cols)
-	m.matrix[0][1] = Fraction{numerator: 1, denominator: 5}
-	m.matrix[1][0] = Fraction{numerator: 5, denominator: 1}
+
+	fmt.Println("Test matrix 1:")
+
+	m.matrix[0][0] = Fraction{numerator: 2, denominator: 1}
+	m.matrix[0][1] = Fraction{numerator: 3, denominator: 1}
+	m.matrix[0][2] = Fraction{numerator: 3, denominator: 1}
+
+	m.matrix[1][0] = Fraction{numerator: 3, denominator: 1}
+	m.matrix[1][1] = Fraction{numerator: 5, denominator: 1}
+	m.matrix[1][2] = Fraction{numerator: 5, denominator: 1}
+
+	m.matrix[2][0] = Fraction{numerator: 3, denominator: 1}
+	m.matrix[2][1] = Fraction{numerator: 5, denominator: 1}
+	m.matrix[2][2] = Fraction{numerator: 5, denominator: 1}
+
 	fmt.Println(m)
+	fmt.Println("------------")
+	fmt.Println(m.rref())
+
+	// ----------------------------------------------------
+
+	fmt.Println("\nTest matrix 2:")
+
+	m.matrix[0][0] = Fraction{numerator: 1, denominator: 1}
+	m.matrix[0][1] = Fraction{numerator: 2, denominator: 1}
+	m.matrix[0][2] = Fraction{numerator: 3, denominator: 1}
+
+	m.matrix[1][0] = Fraction{numerator: 4, denominator: 1}
+	m.matrix[1][1] = Fraction{numerator: 5, denominator: 1}
+	m.matrix[1][2] = Fraction{numerator: 6, denominator: 1}
+
+	m.matrix[2][0] = Fraction{numerator: 7, denominator: 1}
+	m.matrix[2][1] = Fraction{numerator: 8, denominator: 1}
+	m.matrix[2][2] = Fraction{numerator: 9, denominator: 1}
+
+	fmt.Println(m)
+	fmt.Println("------------")
+	fmt.Println(m.rref())
 }
