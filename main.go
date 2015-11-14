2fcc3f7e1ed6e65f2c5a930afdecf31630c4f114
James Jenkins <applesmacs@yahoo.com>
2015-11-13 23:08:06 -0500

    Remove tests from main; Add interactive input
    - The program is now interactive
    - Matrix dimensions and entries can now be entered. Fractions are
      supported as element values
    - rref is performed on inputted matrix if it is square

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
-	//	"strconv"
+	"strconv"
 	"strings"
 )
 
@@ -16,99 +17,85 @@ func prompt(prmpt string) string {
 	return strings.Trim(text, "\n")
 }
 
-func tests() {
-	m := newMatrix(3, 3)
+func getInput() string {
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
 
-	fmt.Println("Test matrix 1:")
+	return strings.Trim(text, "\n")
+}
 
-	m.matrix[0][0] = Fraction{numerator: 2, denominator: 1}
-	m.matrix[0][1] = Fraction{numerator: 3, denominator: 1}
-	m.matrix[0][2] = Fraction{numerator: 3, denominator: 1}
+func getFracInput() (Fraction, error) {
+	fraction := NewFrac(0, 1)
+	var err error
 
-	m.matrix[1][0] = Fraction{numerator: 3, denominator: 1}
-	m.matrix[1][1] = Fraction{numerator: 0, denominator: 1}
-	m.matrix[1][2] = Fraction{numerator: 5, denominator: 1}
+	input := getInput()
+	values := strings.Split(input, "/")
 
-	m.matrix[2][0] = Fraction{numerator: 3, denominator: 1}
-	m.matrix[2][1] = Fraction{numerator: 5, denominator: 1}
-	m.matrix[2][2] = Fraction{numerator: 5, denominator: 1}
+	if len(values) > 0 {
+		fraction.numerator, err = strconv.Atoi(values[0])
+	}
 
-	fmt.Println(m)
-	fmt.Println("------------")
-	fmt.Println(m.rref())
+	if err != nil {
+		return fraction, err
+	}
 
-	// ----------------------------------------------------
+	if len(values) == 2 {
+		fraction.denominator, err = strconv.Atoi(values[1])
+	} else if len(values) > 2 {
+		return fraction, errors.New("Value must be a scalar e.g. 2 or 1/2")
+	}
 
-	fmt.Println("\nTest matrix 2:")
-
-	m.matrix[0][0] = Fraction{numerator: 1, denominator: 1}
-	m.matrix[0][1] = Fraction{numerator: 2, denominator: 1}
-	m.matrix[0][2] = Fraction{numerator: 3, denominator: 1}
-
-	m.matrix[1][0] = Fraction{numerator: 4, denominator: 1}
-	m.matrix[1][1] = Fraction{numerator: 5, denominator: 1}
-	m.matrix[1][2] = Fraction{numerator: 6, denominator: 1}
-
-	m.matrix[2][0] = Fraction{numerator: 7, denominator: 1}
-	m.matrix[2][1] = Fraction{numerator: 8, denominator: 1}
-	m.matrix[2][2] = Fraction{numerator: 9, denominator: 1}
-
-	fmt.Println(m)
-	fmt.Println("------------")
-	fmt.Println(m.rref())
-
-	fmt.Println("\nTest matrix 3:")
-
-	m.matrix[0][0] = Fraction{numerator: 1, denominator: 1}
-	m.matrix[0][1] = Fraction{numerator: 2, denominator: 1}
-	m.matrix[0][2] = Fraction{numerator: 3, denominator: 1}
-
-	m.matrix[1][0] = Fraction{numerator: 1, denominator: 1}
-	m.matrix[1][1] = Fraction{numerator: 2, denominator: 1}
-	m.matrix[1][2] = Fraction{numerator: 4, denominator: 1}
-
-	m.matrix[2][0] = Fraction{numerator: 1, denominator: 1}
-	m.matrix[2][1] = Fraction{numerator: 3, denominator: 1}
-	m.matrix[2][2] = Fraction{numerator: 3, denominator: 1}
-
-	fmt.Println(m)
-	fmt.Println("------------")
-	fmt.Println(m.rref())
+	return fraction, err
 }
 
 func main() {
-	tests()
-	/*
-		var dimensions []string
-		var rows, cols int
+	var dimensions []string
+	var rows, cols int
 
-		for len(dimensions) != 2 {
-			var err1, err2 error
+	// Get input from user for dimensions to use for matrix
+	for len(dimensions) != 2 {
+		var err1, err2 error
 
-			dim_string := prompt("Matrix dimenstions: ")
+		dim_string := prompt("Matrix dimenstions: ")
 
-			// Replace "," with "x" so user can enter J,K or JxK
-			dim_string = strings.Replace(dim_string, ",", "x", 1)
-			dimensions = strings.Split(dim_string, "x")
+		// Replace "," with "x" so user can enter J,K or JxK
+		dim_string = strings.Replace(dim_string, ",", "x", 1)
+		dimensions = strings.Split(dim_string, "x")
 
-			if len(dimensions) != 2 {
-				fmt.Println("Please enter matrix dimensions as JxK.")
-				continue
-			}
-
-			rows, err1 = strconv.Atoi(dimensions[0])
-			cols, err2 = strconv.Atoi(dimensions[1])
-
-			if err1 != nil || err2 != nil {
-				fmt.Println("Please use integers for array dimensions.")
-			}
+		if len(dimensions) != 2 {
+			fmt.Println("Please enter matrix dimensions as JxK.")
+			continue
 		}
 
-		for r := 0; r < rows; r+=1 {
-			for c:=0; c< cols; c+=1 {
+		rows, err1 = strconv.Atoi(dimensions[0])
+		cols, err2 = strconv.Atoi(dimensions[1])
 
+		if err1 != nil || err2 != nil {
+			fmt.Println("Please use integers for array dimensions.")
+		}
+	}
 
-		m := newMatrix(rows, cols)
-		fmt.Println(m)
-	*/
+	m := newMatrix(rows, cols)
+
+	for r := 0; r < rows; r += 1 {
+		for c := 0; c < cols; c += 1 {
+			set := false
+			for set != true {
+				fmt.Printf("Fraction for %d, %d: ", r, c)
+				value, err := getFracInput()
+				if err == nil {
+					m.matrix[r][c] = value
+					set = true
+				} else {
+					fmt.Println(err)
+				}
+			}
+
+		}
+	}
+
+	if rows == cols {
+		m = m.rref()
+	}
+	fmt.Println(m)
 }
